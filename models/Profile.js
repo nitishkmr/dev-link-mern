dcd0241cb3a98910b0eb8743c28ad737813b0807
Nitish Kumar <nitishkmr096@gmail.com>
2021-04-12 00:02:51 +0530

    Add code for add/delete education in profile

--- models/Profile.js
+++ models/Profile.js
@@ -63,7 +63,7 @@ const ProfileSchema = new mongoose.Schema({
                 type: String,
                 required: true
             },
-            fieldOfStudy: {
+            fieldofstudy: {
                 type: String,
                 required: true
             },
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,8 @@ const express = require('express');
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const request = require('request');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
@@ -228,8 +230,8 @@ router.put('/experience',
 
 router.delete('/experience/:exp_id', auth, async (req, res) => {
     try {
-        const profile = await Profile.findOne({ user: req.user.id});
-        
+        const profile = await Profile.findOne({ user: req.user.id });
+
         //Get the index of the exp to be removed
         const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
 
@@ -241,5 +243,108 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
         res.status(500).send("Server Error")
     }
 });
+
+
+
+// Education -> same like experience, an array in Profile Model
+
+// @route   PUT api/profile/education
+// @desc    Add profile education
+// @access  Private - token will be required (private and protected route)
+
+router.put('/education',
+    [
+        auth,
+        [
+            check('school', 'School is required')
+                .not().isEmpty(),
+            check('degree', 'Degree is required')
+                .not().isEmpty(),
+            check('fieldofstudy', 'Field of Study is required')
+                .not().isEmpty(),
+            check('from', 'From date is required')
+                .not().isEmpty(),
+        ]
+    ],
+    async (req, res) => {
+        const error = validationResult(req);
+        if (!error.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { school, degree, fieldofstudy, from, to, current, description } = req.body;
+
+        const newEdu = {
+            school,          // same as title: title
+            degree,
+            fieldofstudy,
+            from,
+            to,
+            current,
+            description
+        };
+
+        //Now connect to DB
+        try {
+            const profile = await Profile.findOne({ user: req.user.id });
+            profile.education.unshift(newEdu);     //same as profile.education.push(newExp).. 
+            // just this one pushes from the start so the latest ones are at the top
+            await profile.save();
+            res.json(profile);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+
+    });
+
+
+// @route   DELETE (could've been PUT also) api/profile/education/:edu_id
+// @desc    Delete education from a profile
+// @access  Private - token will be required (private and protected route)
+
+router.delete('/education/:edu_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        //Get the index of the edu to be removed
+        const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+
+        profile.education.splice(removeIndex, 1);
+        await profile.save();
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error")
+    }
+});
+
+
+
+// @route   GET api/profile/github/:username
+// @desc    Get user repos from Github --- will only work for public repos
+// @access  Public
+router.get('/github/:username', (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&
+            sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            method: 'GET',
+            headers: { 'user-agent': 'node.js' }
+        };
+
+        request(options, (error, response, body) => {
+            if (error) console.error(error);
+            if (response.statusCode != 200) {
+               return res.status(404).json({ msg: 'No Github profile found' });
+            }
+            res.json(JSON.parse(body));
+        })
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error")
+    }
+})
+
 
 module.exports = router;
