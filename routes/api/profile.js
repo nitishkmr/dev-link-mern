89daa0bcf88899d22f16c1dc0106805d6d76f7e0
Nitish Kumar <nitishkmr096@gmail.com>
2021-04-11 12:45:59 +0530

    Add get profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,27 @@
 // adding fetching updating of profiles
 const express = require('express');
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-const router = express.Router();   
+const router = express.Router();
 
-// @route   GET api/profile
-// @desc    Test Route
-// @access  Public (no need of token to be passed here for auth)
-router.get('/', (req, res) => res.send('Profile Route'))
+// @route   GET api/profile/me
+// @desc    Get current user's profile
+// @access  Private (token needs to be passed here for auth for a user)
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile                        //  'user' is from the Schema from Profile model and corresponds to the ObjectID
+            .findOne({ user: req.user.id })                  //  'req.user.id' is from the token being sent / auth on this route
+            .populate('user', ['name', 'avatar']);           // here user, name, avatar came from users.js in models
+        if (!profile) {
+            return res.status(400).json({ msg: 'There is no profile for this user' });
+        }
+        res.json(profile);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
