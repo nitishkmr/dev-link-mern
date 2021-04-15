cd6cd841523f7fc8b79d31519b801ce0999edf58
Nitish Kumar <nitishkmr096@gmail.com>
2021-04-15 23:36:11 +0530

    Make changes for in .json and server.js for deployment

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
-//Connect DB
+// Connect DB
 connectDB();
 
-//Init Middleware
+// Init Middleware
 app.use(express.json({extended: false})); // Now bodyParser is included in express
 
-app.get('/', (req, res) => res.send('API Running'));
-
-//Define Routes
+// Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// after all app.use() only
+// Serve static assests in production
+if(process.env.NODE_ENV === 'production'){
+  // Set static folder
+  app.use(express.static('client/build'));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;       //for heroku
 
